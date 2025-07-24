0815716e5f6c1e58ed78e34515fb4aa9782330f5
potatolain <admin@cpprograms.net>
2025-07-23 20:19:15 -0400

    updates to support vertical layouts instead of telling you to go away

--- dev_server.sh
+++ dev_server.sh
@@ -1,1 +1,1 @@
-npx nodemon . --exec "build_dist.sh && sleep 1 && npx http-server dist" --watch . -e *.js,*.php,*.css,*.html --ignore dist
+npx nodemon . --exec "./build_dist.sh && sleep 1 && npx http-server dist" --watch . -e *.js,*.php,*.css,*.html --ignore dist
